471fb919dfde09c60f8384349d9fad7085a925b2
Bogdan <bogdan.petre@hotmail.com>
2018-09-11 16:48:35 +0100

    Added enrichers, sinks.
    Wired up the configuration from appsettings.json.

--- src/Template.WebAPI/Program.cs
+++ src/Template.WebAPI/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Events;
 
 namespace Template.WebAPI
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static int Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-               .MinimumLevel.Debug()
-               .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-               .Enrich.FromLogContext()
-               .WriteTo.Console()
+               .ReadFrom.Configuration(Configuration)
                .CreateLogger();
 
             try
@@ -29,6 +28,7 @@ namespace Template.WebAPI
 
                 WebHost
                     .CreateDefaultBuilder(args)
+                    .UseConfiguration(Configuration)
                     .UseStartup<Startup>()
                     .UseSerilog()
                     .Build()
