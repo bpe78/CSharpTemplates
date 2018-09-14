044e012da4d999bf7f9e4ebc55bf80b0bc8d9f4f
Bogdan <bogdan.petre@hotmail.com>
2018-09-14 17:10:11 +0100

    Added Automapper.

---
+++ src/Template.WebAPI/MappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+
+namespace Template.WebAPI
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    class MappingConfiguration
+    {
+        public static IMapper Create()
+        {
+            Mapper.Initialize(cfg => {
+                //cfg.CreateMap<Src, Dst>();
+            });
+
+            return Mapper.Instance;
+        }
+    }
+}
