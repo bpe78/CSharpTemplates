922aabb18d357fd60715823c5a27d796521c628e
Bogdan <bogdan.petre@hotmail.com>
2018-10-05 13:25:59 +0100

    Added example console app with generic host.

---
+++ src/Template.ConsoleGenericHost/LifetimeEventsHostedService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Template.ConsoleGenericHost
+{
+    internal class LifetimeEventsHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IApplicationLifetime _appLifetime;
+
+        public LifetimeEventsHostedService(ILogger logger, IApplicationLifetime appLifetime)
+        {
+            _logger = logger.ForContext<LifetimeEventsHostedService>();
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStarted.Register(OnStarted);
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
+
+            //throw new InvalidOperationException("example");   //TODO: this replaces the Serilog.Core.Logger with SilentLogger
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            _logger.Information("OnStarted has been called.");
+
+            // Perform post-startup activities here
+        }
+
+        private void OnStopping()
+        {
+            _logger.Information("OnStopping has been called.");
+
+            // Perform on-stopping activities here
+        }
+
+        private void OnStopped()
+        {
+            _logger.Information("OnStopped has been called.");
+
+            // Perform post-stopped activities here
+        }
+    }
+
+}
---
+++ src/Template.ConsoleGenericHost/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutofacSerilogIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Template.ConsoleGenericHost
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                var host = new HostBuilder()
+                           .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                           .ConfigureHostConfiguration(configHost =>
+                           {
+                               configHost.SetBasePath(Directory.GetCurrentDirectory());
+                               configHost.AddJsonFile("hostsettings.json", optional: true);
+                               //configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                               configHost.AddCommandLine(args);
+                           })
+                           .ConfigureAppConfiguration(configApp =>
+                           {
+                               configApp.AddJsonFile("appsettings.json", optional: false);
+                               //configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                               configApp.AddCommandLine(args);
+                           })
+                           .ConfigureContainer<ContainerBuilder>(builder =>
+                           {
+                               builder.RegisterLogger();
+                               builder.RegisterType<LifetimeEventsHostedService>().As<IHostedService>().SingleInstance();
+                           })
+                           .ConfigureServices((hostContext, services) =>
+                           {
+                           })
+                           .UseSerilog((hostContext, configLogging) =>
+                           {
+                               configLogging.ReadFrom.Configuration(hostContext.Configuration);
+                           })
+                           .UseConsoleLifetime()
+                           .Build();
+
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Web Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
